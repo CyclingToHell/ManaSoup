6cceef9e592212872988a294fcc7c5e9015387db
CyclingToHell <CyclingToHell@users.noreply.github.com>
2022-12-06 19:23:30 +0100

    changes 0.05

--- KrassesGame/Assets/Scripts/Player/PlayerLife.cs
+++ KrassesGame/Assets/Scripts/Player/PlayerLife.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+
+
+    //Leben
+    public int health;
+    public int healthMax;
+
+    [SerializeField] private GameObject healthbar;
+    public Sprite[] life;
+
+
     // Start is called before the first frame update
     private Rigidbody2D rb;
     private Animator anim;
@@ -22,13 +32,33 @@ public class PlayerLife : MonoBehaviour
         
     }
 
+    void Update() 
+    {
+        if( health == healthMax)
+        {   
+            healthbar.GetComponent<SpriteRenderer>().sprite = life[0]; 
+        }
+        else if (health == healthMax - 1 )
+        {   
+            healthbar.GetComponent<SpriteRenderer>().sprite = life[1];
+        }
+        else if (health == healthMax - 2 )
+        {   
+            healthbar.GetComponent<SpriteRenderer>().sprite = life[2];
+        }
+        else
+        {
+            menu();
+        }
+
+    }
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision) 
     {
         if(collision.gameObject.tag == "Gegner")
         {
-            Die();
-            RestartLevel();
+            health = health - 1;
+
         }
         
         if(collision.gameObject.tag == "Ende")
@@ -54,16 +84,6 @@ public class PlayerLife : MonoBehaviour
     {
         SceneManager.LoadScene("Menu");
     }
-
-
-    //IEnumerator Ziel()
-    //{
-        //rb.bodyType = RigidbodyType2D.Static;
-        //anim.SetTrigger("Ziel");
-       // yield return new WaitForSeconds(3f);
-       // SceneManager.LoadScene(SceneManager.LoadScene("xxx"));
-
-    //}
 
    private void RestartLevel()
     {
--- KrassesGame/Assets/Scripts/TimeManager.cs
+++ KrassesGame/Assets/Scripts/TimeManager.cs
@@ -9,7 +9,12 @@ public class TimeManager : MonoBehaviour
     [SerializeField] private int seconds = 20;
     public float timeStart;
     bool timerActive = true;
+
+    
+    public Sprite[] eventTimmy;
     private SpriteRenderer sp;
+
+
 
     //Musik
     
@@ -33,20 +38,19 @@ public class TimeManager : MonoBehaviour
         //Phase 1 
         if(timeStart <= seconds)
         {
-            sp.color = Color.blue;
-            //Musik Wechsel
-            //this.GetComponent<>().audioclip = Phase1;
+            GetComponent<SpriteRenderer>().sprite = eventTimmy[0];
+            
         }
         //Phase 2
         else if(timeStart > seconds && timeStart <= 2*seconds)
         {
-            sp.color = Color.white;
+            GetComponent<SpriteRenderer>().sprite = eventTimmy[1];
             
         }
         //Phase 1 
         else if(timeStart > 2*seconds && timeStart <= 3*seconds)
         {
-            sp.color = Color.green;
+            GetComponent<SpriteRenderer>().sprite = eventTimmy[2];
             
         }
        
@@ -54,7 +58,7 @@ public class TimeManager : MonoBehaviour
         else if (timeStart > 3*seconds)
         {
             timeStart = 0;
-            sp.color = Color.blue;
+            
             print("Error");
         }
 
